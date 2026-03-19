1a993507a872c8882160d667a213821a09626b91
unknwn <73308423+f1w@users.noreply.github.com>
2026-03-19 18:01:01 +0100

    Refactor region constants for Southeast Asia
    Updated region constants and deprecated old regions for compatibility.

--- api/constants.go
+++ api/constants.go
@@ -12,17 +12,21 @@ const (
 	RegionKorea             Region = "kr"
 	RegionLatinAmericaNorth Region = "la1"
 	RegionLatinAmericaSouth Region = "la2"
+	RegionMiddleEast        Region = "me1"
 	RegionNorthAmerica      Region = "na1"
 	RegionOceania           Region = "oc1"
 	RegionPBE               Region = "pbe1"
-	RegionPhilippines       Region = "ph2"
-	RegionRussia            Region = "ru"
-	RegionSingapore         Region = "sg2"
-	RegionThailand          Region = "th2"
-	RegionTurkey            Region = "tr1"
-	RegionTaiwan            Region = "tw2"
-	RegionVietnam           Region = "vn2"
-	RegionMena              Region = "me1"
+	// Deprecated: Use api.RegionSouthEastAsia instead. PH2 got merged into the SEA server on 8th of Jan, 2025.
+	RegionPhilippines Region = "sg2" // PH2 got merged into SG2 on Jan. 8th, 2025. Replaced for backwards compatability.
+	RegionRussia      Region = "ru"
+	// Deprecated: Use api.RegionSouthEastAsia instead. SG2 is now called SEA
+	RegionSingapore     Region = "sg2" // SG2 is now called SEA while still running on SG2.
+	RegionSouthEastAsia Region = "sg2"
+	// Deprecated: Use api.RegionSouthEastAsia instead. TH2 got merged into the SEA server on 8th of Jan, 2025.
+	RegionThailand Region = "sg2" // TH2 got merged into SG2 on Jan. 8th, 2025. Replaced for backwards compatability.
+	RegionTurkey   Region = "tr1"
+	RegionTaiwan   Region = "tw2"
+	RegionVietnam  Region = "vn2"
 )
 
 // Route represents a server region's route
@@ -47,16 +51,14 @@ var (
 		RegionLatinAmericaNorth,
 		RegionLatinAmericaSouth,
 		RegionNorthAmerica,
+		RegionMiddleEast,
 		RegionOceania,
 		RegionPBE,
-		RegionPhilippines,
 		RegionRussia,
-		RegionSingapore,
-		RegionThailand,
+		RegionSouthEastAsia,
 		RegionTurkey,
 		RegionTaiwan,
 		RegionVietnam,
-		RegionMena,
 	}
 
 	// RegionToRoute maps each region to its route
@@ -68,15 +70,13 @@ var (
 		RegionKorea:             RouteAsia,
 		RegionLatinAmericaNorth: RouteAmericas,
 		RegionLatinAmericaSouth: RouteAmericas,
+		RegionMiddleEast:        RouteEurope,
 		RegionNorthAmerica:      RouteAmericas,
 		RegionOceania:           RouteSEA,
-		RegionPhilippines:       RouteSEA,
 		RegionRussia:            RouteEurope,
-		RegionSingapore:         RouteSEA,
-		RegionThailand:          RouteSEA,
+		RegionSouthEastAsia:     RouteSEA,
 		RegionTurkey:            RouteEurope,
 		RegionTaiwan:            RouteSEA,
 		RegionVietnam:           RouteSEA,
-		RegionMena:              RouteEurope,
 	}
 )
